7c1a2fbde7478862fd0ea3f91da35d6f6bba83c5
Frank DiMuzio <frankd@t470s.local>
2020-05-27 09:01:45 -0400

    updated some polybar files

--- polybar_launch.sh
+++ polybar_launch.sh
@@ -7,6 +7,12 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar example &
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
 
 echo "Polybar launched..."
